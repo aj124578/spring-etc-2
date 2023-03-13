4248cf2ca58739564cc6746eb88eed982a71e47b
aj124578 <aj124578@naver.com>
2023-03-13 17:39:07 +0900

    @LoginAdvice v2

--- src/main/java/shop/mtcoding/springetc2/handler/LoginAdvice.java
+++ src/main/java/shop/mtcoding/springetc2/handler/LoginAdvice.java
@@ -1,39 +1,53 @@
 package shop.mtcoding.springetc2.handler;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import shop.mtcoding.springetc2.handler.aop.LoginUser;
 import shop.mtcoding.springetc2.model.User;
 
 @Aspect
 @Component
 public class LoginAdvice {
 
-    @Around("execution(* shop.mtcoding.springetc2.controller..*.*(..))")
-    public Object loginUserAdvice(ProceedingJoinPoint jp) throws Throwable {
-        Object result = jp.proceed();
-        Object[] param = new Object[1];
-        Object[] args = jp.getArgs();
+    private ParameterNameDiscoverer parameterNameDiscoverer = new DefaultParameterNameDiscoverer();
 
-        System.out.println("테스트 : " + args.length);
-        for (Object arg : args) {
-            if (arg instanceof User) {
-                HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
-                        .getRequest();
-                HttpSession session = req.getSession();
-                User principal = (User) session.getAttribute("principal");
-                param[0] = principal;
-                result = jp.proceed(param);
+    @Around("execution(* shop.mtcoding.aopstudy.controller..*.*(..))")
+    public Object loginUserAdvice(ProceedingJoinPoint jp) throws Throwable {
+        Object[] args = jp.getArgs(); // 메서드마다 10개, 100개 몇개인줄 모르니까 Object 타입의 배열로 받음
+
+        MethodSignature signature = (MethodSignature) jp.getSignature();
+        Method method = signature.getMethod();
+        
+        Annotation[][] annotationsPA = method.getParameterAnnotations();
+
+        for (int i = 0; i < args.length; i++) {
+            Annotation[] annotations = annotationsPA[i]; // 첫번째 파라메터의 어노테이션, 두번째 파라메터의 어노테이션
+            for (Annotation anno : annotations) {
+                if (anno instanceof LoginUser) {
+                    HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                            .getRequest();
+                    HttpSession session = req.getSession();
+                    User principal = (User) session.getAttribute("principal");
+                    return jp.proceed(new Object[] { principal });
+                }
             }
         }
 
-        return result;
+        return jp.proceed();
     }
+
 }
